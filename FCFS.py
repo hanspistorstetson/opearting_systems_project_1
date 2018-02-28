43962d8b167c330b5d82b013360c6d7f90487521
Hans Pistor <hpistor@stetson.edu>
2018-02-28 11:06:43 -0500

    refactor

--- FCFS.py
+++ FCFS.py
@@ -1,35 +1,38 @@
 import random
 
 
-def findWaitingTime(processes, n, bt, wt):
-    wt[0] = 0
-    for i in range(1, n):
-        wt[i] = bt[i - 1] + wt[i - 1]
+class FCFSProcesses:
+    def __init__(self, n=900, q=1):
+        self.processes = [x for x in range(1, n + 1)]
+        self.bt = [random.randrange(1, 100) for x in range(1, n + 1)]
+        self.n = n
+
+    def findWaitingTime(self):
+        self.wt[0] = 0
+        for i in range(1, self.n):
+            self.wt[i] = self.bt[i - 1] + self.wt[i - 1]
+
+    def findTurnaroundTime(self):
+        for i in range(0, self.n):
+            self.tt[i] = self.wt[i] + self.bt[i]
+
+    def findAvgTime(self):
+        self.wt = [0 for x in range(len(self.processes))]
+        self.tt = [0 for x in range(len(self.processes))]
+        self.total_wait = 0
+        self.total_turnaround = 0
+        self.findWaitingTime()
+        self.findTurnaroundTime()
+        print("Process ID\tBurst Time\tWaiting Time\tTurnaround Time")
+        for i in range(0, self.n):
+            self.total_wait += self.wt[i]
+            self.total_turnaround += self.tt[i]
+            print(str(self.processes[i]) + "\t\t" + str(self.bt[i]) + "\t\t" +
+                  str(self.wt[i]) + "\t\t" + str(self.tt[i]))
+        print("\n\nAverage waiting time:\t\t" + str(self.total_wait / self.n))
+        print("Average turnaround time:\t" +
+              str(self.total_turnaround / self.n))
 
 
-def findTurnaroundTime(processes, n, bt, wt, tat):
-    for i in range(0, n):
-        tat[i] = wt[i] + bt[i]
-
-
-def findAvgTime(processes, n, bt):
-    waiting_time = [0 for x in range(len(processes))]
-    turnaround_time = [0 for x in range(len(processes))]
-    total_wait = 0
-    total_turnaround = 0
-    findWaitingTime(processes, n, bt, waiting_time)
-    findTurnaroundTime(processes, n, bt, waiting_time, turnaround_time)
-    print("Process ID\tBurst Time\tWaiting Time\tTurnaround Time")
-    for i in range(0, n):
-        total_wait += waiting_time[i]
-        total_turnaround += turnaround_time[i]
-        print(str(processes[i]) + "\t\t" + str(bt[i]) + "\t\t" +
-              str(waiting_time[i]) + "\t\t" + str(turnaround_time[i]))
-    print("\n\nAverage waiting time:\t\t" + str(total_wait / n))
-    print("Average turnaround time:\t" + str(total_turnaround / n))
-
-
-processes = [x for x in range(1, 1001)]
-burst_time = [random.randrange(1, 100) for x in range(1, 1001)]
-n = len(processes)
-findAvgTime(processes, n, burst_time)
+processes = FCFSProcesses()
+processes.findAvgTime()
--- RR.py
+++ RR.py
@@ -1,48 +1,51 @@
 import random
 
 
-def findWaitingTime(processes, n, bt, wt, q):
-    rem = [x for x in bt]
-    t = 0
-    done = False
-    while not done:
-        done = True
-        for i in range(0, n):
-            if (rem[i] > 0):
-                done = False
-                if (rem[i] > q):
-                    t += q
-                    rem[i] -= q
-                else:
-                    t += rem[i]
-                    wt[i] = t - bt[i]
-                    rem[i] = 0
+class RRProcesses:
+    def __init__(self, n=900, q=1):
+        self.processes = [x for x in range(1, n + 1)]
+        self.bt = [random.randrange(1, 100) for x in range(1, n + 1)]
+        self.n = n
+        self.q = q
+
+    def findWaitingTime(self):
+        rem = [x for x in self.bt]
+        t = 0
+        done = False
+        while not done:
+            done = True
+            for i in range(0, self.n):
+                if (rem[i] > 0):
+                    done = False
+                    if (rem[i] > self.q):
+                        t += self.q
+                        rem[i] -= self.q
+                    else:
+                        t += rem[i]
+                        self.wt[i] = t - self.bt[i]
+                        rem[i] = 0
+
+    def findTurnaroundTime(self):
+        for i in range(0, self.n):
+            self.tt[i] = self.wt[i] + self.bt[i]
+
+    def findAvgTime(self):
+        self.wt = [0 for x in range(len(self.processes))]
+        self.tt = [0 for x in range(len(self.processes))]
+        self.total_wait = 0
+        self.total_turnaround = 0
+        self.findWaitingTime()
+        self.findTurnaroundTime()
+        print("Process ID\tBurst Time\tWaiting Time\tTurnaround Time")
+        for i in range(0, self.n):
+            self.total_wait += self.wt[i]
+            self.total_turnaround += self.tt[i]
+            print(str(self.processes[i]) + "\t\t" + str(self.bt[i]) + "\t\t" +
+                  str(self.wt[i]) + "\t\t" + str(self.tt[i]))
+        print("\n\nAverage waiting time:\t\t" + str(self.total_wait / self.n))
+        print("Average turnaround time:\t" +
+              str(self.total_turnaround / self.n))
 
 
-def findTurnaroundTime(processes, n, bt, wt, tat):
-    for i in range(0, n):
-        tat[i] = wt[i] + bt[i]
-
-
-def findAvgTime(p, n, bt, q):
-    wt = [0 for x in range(len(processes))]
-    tt = [0 for x in range(len(processes))]
-    total_wait = 0
-    total_turnaround = 0
-    findWaitingTime(processes, n, bt, wt, q)
-    findTurnaroundTime(processes, n, bt, wt, tt)
-    print("Process ID\tBurst Time\tWaiting Time\tTurnaround Time")
-    for i in range(0, n):
-        total_wait += wt[i]
-        total_turnaround += tt[i]
-        print(str(processes[i]) + "\t\t" + str(bt[i]) + "\t\t" +
-              str(wt[i]) + "\t\t" + str(tt[i]))
-    print("\n\nAverage waiting time:\t\t" + str(total_wait / n))
-    print("Average turnaround time:\t" + str(total_turnaround / n))
-
-
-processes = [x for x in range(1, 1001)]
-burst_time = [random.randrange(1, 100) for x in range(1, 1001)]
-n = len(processes)
-quantum = 2
-findAvgTime(processes, n, burst_time, quantum)
+processes = RRProcesses()
+processes.findAvgTime()
